dcf61be7dc51dac9b61fce7c474fd3a3d7d35997
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-11-01 12:29:35 -0200

    Add new aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,10 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+alias p8="ping 8.8.8.8"
+alias pg="ping google.com.br"
+alias showHidden='defaults write com.apple.finder AppleShowAllFiles YES;
+killall Finder /System/Library/CoreServices/Finder.app'
+alias hideHidden='defaults write com.apple.finder AppleShowAllFiles NO;
+killall Finder /System/Library/CoreServices/Finder.app'
